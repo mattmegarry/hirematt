5787e90e15c785b7a2efa6b9f1f6f2c29a0943ef
Matt Megarry <mattmegarry@gmail.com>
2023-05-08 13:23:38 +0100

    basic templating for any number of pages

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-const { readFile, writeFile } = require('fs/promises')
+const { readFile, writeFile, readdir } = require('fs/promises')
 
 async function getFileString(path) {
     return await readFile(path, 'utf8')
@@ -9,18 +9,21 @@ async function overwriteStringToFile(path, string) {
 }
 
 (async function main() {
-    const indexFilePath = './dist/index.html'
-    const indexContent = await getFileString(indexFilePath)
+    const outputDirPath = './dist/'
+    const pagesDirPath = './dist/pages'
+    const pagesDirFiles = await readdir(pagesDirPath)
 
-    const templateFilePath = './dist/template.html'
+    const templateFilePath = './dist/templates/template.html'
     const templateContent = await getFileString(templateFilePath)
 
-    fullIndexPage = templateContent.replace(
-        '{% main_content %}',
-        indexContent
-    )
-
-    await overwriteStringToFile(indexFilePath, fullIndexPage)
+    pagesDirFiles.forEach(async (filename) => {
+        const pageContent = await getFileString("./dist/pages/" + filename)
+        const fullPageContent = templateContent.replace(
+            '{% main_content %}',
+            pageContent
+        )
+        await overwriteStringToFile(outputDirPath + filename, fullPageContent)
+    })
 })();
 
 
